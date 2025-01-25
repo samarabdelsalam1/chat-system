569a2ee5a37b4c7f1a5c7b7b7871d4d417fab36b
samarabdelsalam1 <samar.abdelsalam@espace.com.eg>
2025-01-25 17:14:50 +0200

    fix entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+until mysql -h chat-system_development_db -u ${DEVELOPMENT_DB_USERNAME} -p${DEVELOPMENT_DB_PASSWORD} -e "SELECT 1"; do
+  echo "Waiting for database to be ready..."
+  sleep 5
+done
+
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
-
+echo "Running bundle install..."
 bundle install
+
+echo "Running migrations..."
 rails db:migrate
-rails db:seed
+
+echo "Starting server..."
 exec "$@"
